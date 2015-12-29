52c8ebbd08108eebc2aefbc9b50efcd48fa097c2
jizp <jizp@hyron-js.com>
2015-12-29 19:40:55 +0800

    update

--- IOSD/app/src/main/java/com/example/jizp/iosd/MainActivity.java
+++ IOSD/app/src/main/java/com/example/jizp/iosd/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.jizp.iosd;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -25,6 +28,9 @@ public class MainActivity extends Activity {
 
     private static final String FILENAME = "data.txt";
 
+    private static final int REQUEST_CODE_ASK_PERMISSIONS_WRITE = 1;
+    private static final int REQUEST_CODE_ASK_PERMISSIONS_READ = 2;
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,122 +39,131 @@ public class MainActivity extends Activity {
         btnSave = (Button) findViewById(R.id.save);
         btnRead = (Button) findViewById(R.id.read);
 
+        btnSave.setOnClickListener(new SaveOnClickListener());
+        btnRead.setOnClickListener(new ReadOnClickListener());
+    }
 
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PrintStream ps = null;
+    private class SaveOnClickListener implements View.OnClickListener {
+        public void onClick(View v) {
 
-/*                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    //申请WRITE_EXTERNAL_STORAGE权限
-                    Activity.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                            WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
-
-                    Activity.
-                }*/
-
-
-                if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-                    Toast.makeText(getApplicationContext(), "SD card error...", Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                String dir = Environment.getExternalStorageDirectory().getAbsolutePath().toString() + File.separator + "kkk";
-
-                //file = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_NOTIFICATIONS);
-                file = new File(dir, FILENAME);
-
-
-                if (!file.exists()) {
-                    file.mkdirs();
-                }
-
-                /*String filetmp = filepath.getAbsolutePath() + File.separator + FILENAME;
-                try {
-                    String filetmp1 = filepath.getCanonicalPath() + File.separator + FILENAME;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }*/
-
-                //file = new File(filepath);
-
-                if (!file.exists()) {
-                    try {
-                        file.createNewFile();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                try {
-                    FileOutputStream fileOutputStream = new FileOutputStream(file);
-                    //FileOutputStream fileOutputStream = openFileOutput(path, BIND_AUTO_CREATE);
-                    ps = new PrintStream(fileOutputStream);
-                    ps.println("网址：www.kkk.com");
-                    ps.println("电子邮件：k@kkk.com");
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } finally {
-                    ps.close();
-                }
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_ASK_PERMISSIONS_WRITE);
+                return;
             }
-        });
 
-        btnRead.setOnClickListener(new View.OnClickListener() {
-                                       @Override
-                                       public void onClick(View v) {
+            SaveFile();
+        }
+    }
 
-                                           StringBuffer info = new StringBuffer();
+    private class ReadOnClickListener implements View.OnClickListener {
 
-                                           if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-                                               Toast.makeText(getApplicationContext(), "SD card error...", Toast.LENGTH_LONG).show();
-                                               return;
-                                           }
+        @Override
+        public void onClick(View v) {
 
-                                           String path = Environment.getExternalStorageDirectory().toString() + File.separator + "kkk" + File.separator + FILENAME;
-                                           file = new File(path);
 
-                                           if (!file.exists()) {
-                                               Toast.makeText(getApplicationContext(), "File is not exist...", Toast.LENGTH_LONG).show();
-                                               return;
-                                           }
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_ASK_PERMISSIONS_READ);
+                return;
+            }
 
-                                           Scanner scanner = null;
+            ReadFile();
+        }
+    }
 
-                                           try {
-                                               scanner = new Scanner(new FileInputStream(file));
-                                               while (scanner.hasNext()) {
-                                                   info.append(scanner.next()).append("★★★\n");
-                                               }
-                                               Toast.makeText(getApplicationContext(), info.toString(), Toast.LENGTH_LONG).show();
-                                           } catch (FileNotFoundException e) {
-                                               e.printStackTrace();
-                                           } finally {
-                                               scanner.close();
-                                           }
-                                       }
+    private void SaveFile() {
+        PrintStream ps = null;
 
-                                   }
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            Toast.makeText(getApplicationContext(), "SD card error...", Toast.LENGTH_LONG).show();
+            return;
+        }
 
-        );
+        String dir = Environment.getExternalStorageDirectory().getAbsolutePath().toString() + File.separator + "kkk";
+
+        file = new File(dir, FILENAME);
+
+
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            ps = new PrintStream(fileOutputStream);
+            ps.println("网址：www.kkk.com");
+            ps.println("电子邮件：k@kkk.com");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            ps.close();
+        }
+    }
+
+    private void ReadFile() {
+
+        StringBuffer info = new StringBuffer();
+
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            Toast.makeText(getApplicationContext(), "SD card error...", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String path = Environment.getExternalStorageDirectory().toString() + File.separator + "kkk" + File.separator + FILENAME;
+        file = new File(path);
+
+        if (!file.exists()) {
+            Toast.makeText(getApplicationContext(), "File is not exist...", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(new FileInputStream(file));
+            while (scanner.hasNext()) {
+                info.append(scanner.next()).append("★★★\n");
+            }
+            Toast.makeText(getApplicationContext(), info.toString(), Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            scanner.close();
+        }
+
     }
 
 
-/*    @Override
+    @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
-            case REQUEST_CODE_ASK_PERMISSIONS:
+            case REQUEST_CODE_ASK_PERMISSIONS_WRITE:
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Permission Granted
-                    insertDummyContact();
+
+                    SaveFile();
+
                 } else {
-                    // Permission Denied
-                    Toast.makeText(MainActivity.this, "WRITE_CONTACTS Denied", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "PERMISSION WRITE ERROR", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case REQUEST_CODE_ASK_PERMISSIONS_READ:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    ReadFile();
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "PERMISSION READ ERROR", Toast.LENGTH_LONG).show();
                 }
                 break;
             default:
                 super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-    }*/
+    }
 }
